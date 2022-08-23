bfa50a9cad9d777efe3d1b90f6f187300468d896
Alex <milleniumfalcon_@mail.ru>
2022-08-23 11:36:01 +0300

    Update for wisiwig panel

--- shiftedblog/blog/admin.py
+++ shiftedblog/blog/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
+
 from blog import models
+from blog.forms import PostAdminForm
 
 
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    class Media:
-        css = {
-            "all": ("css/custom_admin.css",)
-        }
-        js = ("js/tinymce.js",)
+    form = PostAdminForm
 
     list_display = ('title', 'slug', 'author', 'published', 'status')
     list_filter = ('status', 'created', 'published', 'author')
--- shiftedblog/blog/forms.py
+++ shiftedblog/blog/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 
+from blog import models
+
 
 class SearchForm(forms.Form):
     query = forms.CharField()
+
+
+class PostAdminForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea(attrs={'id': "richtext_field"}))
+
+    class Meta:
+        model = models.Post
+        fields = "__all__"
