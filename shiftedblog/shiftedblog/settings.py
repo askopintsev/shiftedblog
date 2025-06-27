12f22939c02660e52834e5b2abb6cb5f68e112ff
Alex <milleniumfalcon_mail.ru>
2025-06-27 16:19:34 +0300

    update editor in admin

--- shiftedblog/shiftedblog/settings.py
+++ shiftedblog/shiftedblog/settings.py
@@ -205,60 +205,39 @@ X_FRAME_OPTIONS = 'DENY'
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': {
-            'items': ['heading', '|', 'bold', 'italic', 'link',
-                        'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
-                    }
-
-    },
-    'extends': {
-        'blockToolbar': [
-            'paragraph', 'heading1', 'heading2', 'heading3',
-            '|',
-            'bulletedList', 'numberedList',
-            '|',
-            'blockQuote',
-        ],
-        'toolbar': {
-            'items': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough',
-                        'code','subscript', 'superscript', 'highlight', '|', 'codeBlock', 'sourceEditing', 'insertImage',
-                    'bulletedList', 'numberedList', 'todoList', '|',  'blockQuote', 'imageUpload', '|',
-                    'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
-                    'insertTable',
-                    ],
-            'shouldNotGroupWhenFull': 'true'
+            'items': [
+                'heading', '|',
+                'Bold', 'Italic', 'Underline', 'Strikethrough', 'code', 'Subscript', 
+                'Superscript',  'specialCharacters', 'highlight', 'RemoveFormat', '|',
+                'NumberedList', 'BulletedList', 'todoList', '|',
+                'Blockquote', '|',
+                'codeBlock', '|',
+                'alignment', 'Outdent', 'Indent', '|',
+                'insertImage', '|',
+                'Link', 'Unlink', '|',
+                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
+                'mediaEmbed',
+                'insertTable',
+                'HorizontalLine',
+                'sourceEditing', '|',
+                'undo', 'redo',
+            ],
+            "shouldNotGroupWhenFull": True,
         },
         'image': {
-            'toolbar': ['imageTextAlternative', '|', 'imageStyle:alignLeft',
-                        'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side',  '|'],
-            'styles': [
-                'full',
-                'side',
-                'alignLeft',
-                'alignRight',
-                'alignCenter',
-            ]
-
+            'toolbar': [
+                'imageStyle:full', 'imageStyle:side', 'imageStyle:alignLeft',
+                'imageStyle:alignCenter', 'imageStyle:alignRight', 'toggleImageCaption',
+                'imageTextAlternative',
+            ],
         },
         'table': {
-            'contentToolbar': [ 'tableColumn', 'tableRow', 'mergeTableCells',
-            'tableProperties', 'tableCellProperties' ],
+            'contentToolbar': [
+                'tableColumn', 'tableRow', 'mergeTableCells',
+                'tableProperties', 'tableCellProperties',
+            ],
         },
-        'heading' : {
-            'options': [
-                { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
-                { 'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1' },
-                { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
-                { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' }
-            ]
-        }
     },
-    'list': {
-        'properties': {
-            'styles': 'true',
-            'startIndex': 'true',
-            'reversed': 'true',
-        }
-    }
 }
 
 # Define a constant in settings.py to specify file upload permissions
